2adc101ed3ec3f28ab00c2a01bec1381f297b0b7
Ducvietbui <e1700690@edu.vamk.fi>
2018-04-03 10:39:53 +0000

    tenth commit

--- screen.h
+++ screen.h
@@ -1,3 +1,5 @@
+#define UNICODE		//use unicode to make good-looking bar
+#define BAR "\u2590"
 // constant definitions
 #define ESC 0x1B
 enum COLORS {BLACK=30, RED, GREEN, YELLOW, BLUE, MAGNETA, CYAN, WHITE};

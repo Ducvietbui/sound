c2b5b5b949e52fb9127ec57cc06d6debeafdff09
Ducvietbui <e1700690@edu.vamk.fi>
2018-04-10 08:26:22 +0000

    eighth commit

--- screen.c
+++ screen.c
@@ -1,12 +1,24 @@
 #include "screen.h"
 #include <stdio.h>
-
+/*
+	function definition of clearScreen()
+	This function uses VT100 escape sequence \ESC[2J to make the whole
+	terminal screen empty
+	argument : no
+	return : no
+*/
 void 	clearScreen(void){
 	printf("%c[2J", ESC);
 	fflush(stdout);
 }
 
-// function definition of gotoxy()
+/* 	function definition of gotoxy()
+	This function uses VT100 escape sequence \ESC[row;colH to set cursor
+	to a specific location on the terminal screen
+	argument:	row number, 1 is top row
+			col number, 1 is left column
+	return : no
+*/
 void 	gotoxy(int row, int col){
 	printf("%c[%d;%dH", ESC, row, col);
 	fflush(stdout);
